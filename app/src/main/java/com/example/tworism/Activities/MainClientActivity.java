123ad5060bce51e4ddcc31cbeda0117151ba59bd
Empanada <ronald362001@gmail.com>
2022-09-04 14:38:08 -0600

    Update user

--- app/src/main/java/com/example/tworism/Activities/MainClientActivity.java
+++ app/src/main/java/com/example/tworism/Activities/MainClientActivity.java
@@ -4,9 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.example.tworism.Adapter.RecentsAdapter;
+import com.example.tworism.MainActivity;
 import com.example.tworism.Models.RecentsModel;
 import com.example.tworism.R;
 
@@ -14,17 +18,23 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.example.tworism.R;
+import com.example.tworism.Users.ProfileActivity;
 
 public class MainClientActivity extends AppCompatActivity {
 
     //RecyclerViews
     RecyclerView recentRecycler;
     RecentsAdapter recentsAdapter;
+    ImageView profile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_client);
+        String UserId = getIntent().getStringExtra("UserId");
+        String UserName = getIntent().getStringExtra("UserName");
+
+        profile = findViewById(R.id.profile);
 
         //Datos  de prueba para recents recyclerView
         List<RecentsModel> recentsDataList = new ArrayList<>();
@@ -38,6 +48,17 @@ public class MainClientActivity extends AppCompatActivity {
 
 
         setRecentRecycler(recentsDataList);
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainClientActivity.this, ProfileActivity.class);
+                intent.putExtra("UserId", UserId);
+                intent.putExtra("UserName", UserName);
+                intent.putExtra("Procedure", "Cliente");
+                startActivity(intent);
+            }
+        });
     }
 
     private void setRecentRecycler(List<RecentsModel> recentsDataList){
@@ -48,5 +69,11 @@ public class MainClientActivity extends AppCompatActivity {
         recentsAdapter = new RecentsAdapter(this, recentsDataList);
         recentRecycler.setAdapter(recentsAdapter);
 
+    }
+
+    public void onBackPressed(){
+        Intent intent = new Intent(MainClientActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
