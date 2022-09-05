5f7e2e66d13f038d2aa78292693d7ffef72f1bd0
Reymor <nmorales895@gmail.com>
2022-09-04 18:52:55 -0600

    "Preview details added"

--- app/src/main/java/com/example/tworism/Activities/MainClientActivity.java
+++ app/src/main/java/com/example/tworism/Activities/MainClientActivity.java
@@ -5,46 +5,67 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
-import com.example.tworism.Adapter.RecentsAdapter;
-import com.example.tworism.Models.RecentsModel;
+import com.example.tworism.Adapter.RecentsDataAdapter;
 import com.example.tworism.R;
+import com.example.tworism.Retrofit.RecentsDataModel;
+import com.example.tworism.Retrofit.RecentsInterface;
+import com.example.tworism.Retrofit.VehicleInterface;
+import com.example.tworism.RetrofitClient;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
 public class MainClientActivity extends AppCompatActivity {
 
-    //RecyclerViews
     RecyclerView recentRecycler;
-    RecentsAdapter recentsAdapter;
+    ProgressBar progressBar;
+    LinearLayoutManager layoutManager;
+    RecentsDataAdapter recentsDataAdapter;
+    List<RecentsDataModel> recentsDataModelList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_client);
 
-        //Datos  de prueba para recents recyclerView
-        List<RecentsModel> recentsDataList = new ArrayList<>();
-        recentsDataList.add(new RecentsModel("El cuco","San Miguel","35"));
-        recentsDataList.add(new RecentsModel("Volcan chaparrastique","San Miguel","35"));
-        recentsDataList.add(new RecentsModel("Catedral","San Miguel","35"));
-        recentsDataList.add(new RecentsModel("Un nombre largo de prueba","San Miguel","35"));
-        recentsDataList.add(new RecentsModel("Indica el nombre aqui por favor","San salvador","35"));
-        recentsDataList.add(new RecentsModel("El cuco","San Miguel","Desde 35 hasta 85 y todo denevhfv"));
-        recentsDataList.add(new RecentsModel("El cuco","San Miguel","35"));
-
-
-        setRecentRecycler(recentsDataList);
-    }
-
-    private void setRecentRecycler(List<RecentsModel> recentsDataList){
-
         recentRecycler = findViewById(R.id.recent_recycler);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false );
+        progressBar = findViewById(R.id.progressBar);
+        layoutManager = new LinearLayoutManager(this);
         recentRecycler.setLayoutManager(layoutManager);
-        recentsAdapter = new RecentsAdapter(this, recentsDataList);
-        recentRecycler.setAdapter(recentsAdapter);
+        recentsDataAdapter =new RecentsDataAdapter(recentsDataModelList);
+        recentRecycler.setAdapter(recentsDataAdapter);
 
+        fetchRecentData();
     }
+
+    public void fetchRecentData(){
+        RecentsInterface api = RetrofitClient.getClient().create(RecentsInterface.class);
+        api.getRecentsDataModel().enqueue(new Callback<List<RecentsDataModel>>() {
+            @Override
+            public void onResponse(Call<List<RecentsDataModel>> call, Response<List<RecentsDataModel>> response) {
+                if (response.isSuccessful() && response.body()!=null){
+                    recentsDataModelList.addAll(response.body());
+                    recentsDataAdapter.notifyDataSetChanged();
+                    progressBar.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<RecentsDataModel>> call, Throwable t) {
+                progressBar.setVisibility(View.GONE);
+                Toast.makeText(MainClientActivity.this, "Error"+t.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
 }
