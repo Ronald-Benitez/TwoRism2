f4dc5f1de5f3e469dc28d1b2d897b9dd9ceada22
Ceopht <moisesbeltran8930@gmail.com>
2022-09-04 20:14:27 -0600

    aaaaa

--- app/src/main/java/com/example/tworism/Provider/ProviderMainActivity.java
+++ app/src/main/java/com/example/tworism/Provider/ProviderMainActivity.java
@@ -1,19 +1,19 @@
 package com.example.tworism.Provider;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.tworism.ListarVehiculos;
 import com.example.tworism.R;
 import com.example.tworism.RegistrarVehiculo;
 
 public class ProviderMainActivity extends AppCompatActivity {
-    Button btnAddVehicle,btnListarVehiculo;
+    Button btnAddVehicle,btnListarVehiculo,btnOrganizarViaje;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +25,7 @@ public class ProviderMainActivity extends AppCompatActivity {
 
         btnAddVehicle = findViewById(R.id.btnAddVehicle);
         btnListarVehiculo = findViewById(R.id.btnListarVehiculo);
+        btnOrganizarViaje = findViewById(R.id.btnOrganizarViaje);
 
         if(!UserVerified){
             AlertDialog.Builder builder = new AlertDialog.Builder(ProviderMainActivity.this);
@@ -51,6 +52,17 @@ public class ProviderMainActivity extends AppCompatActivity {
                 Intent intent = new Intent(ProviderMainActivity.this, ListarVehiculos.class);
                 intent.putExtra("UserId", UserId);
                 intent.putExtra("UserName", UserName);
+                startActivity(intent);
+            }
+        });
+
+        btnOrganizarViaje.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ProviderMainActivity.this, OrganizarViaje.class);
+                intent.putExtra("UserId", UserId);
+                intent.putExtra("UserName", UserName);
+                intent.putExtra("UserVerified", UserVerified.toString());
                 startActivity(intent);
             }
         });
